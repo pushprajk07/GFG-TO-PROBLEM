15fe6703d33e1534aa68c15238e9a218ec841d94
pushprajk07 <pushprajkpr7@gmail.com>
2025-03-25 01:02:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max Score from Subarray Mins/max-score-from-subarray-mins.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = Integer.parseInt(scanner.nextLine());
+
+        while (t-- > 0) {
+            String line = scanner.nextLine();
+            String[] tokens = line.split(" ");
+            List<Integer> nums = new ArrayList<>();
+            for (String token : tokens) {
+                nums.add(Integer.parseInt(token));
+            }
+            int[] arr = new int[nums.size()];
+            int idx = 0;
+            for (int i : nums) arr[idx++] = i;
+            Solution solution = new Solution();
+            System.out.println(solution.pairWithMaxSum(arr));
+
+            System.out.println("~");
+        }
+
+        scanner.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to find pair with maximum sum
+    public int pairWithMaxSum(int arr[]) {
+        // Your code goes here
+        int maxSum = Integer.MIN_VALUE;
+        for (int i=1;i<arr.length;i++){
+            maxSum = Math.max(maxSum,arr[i-1]+arr[i]);
+        }
+        return maxSum;
+    }
+}
