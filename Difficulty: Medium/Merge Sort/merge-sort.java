6453c8327c2475cee39a049d362afbbeb3aed311
pushprajk07 <pushprajkpr7@gmail.com>
2025-08-17 16:06:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort/merge-sort.java
+++ Difficulty: Medium/Merge Sort/merge-sort.java
@@ -1,42 +1,50 @@
-import java.util.Arrays;
-
 class Solution {
 
     void mergeSort(int arr[], int l, int r) {
-        if (l < r) {
-            int mid = l + (r - l) / 2;
-            mergeSort(arr, l, mid);
-            mergeSort(arr, mid + 1, r);
-            merge(arr, l, mid, r);
+        // code here
+        if(l>=r){
+            return;
         }
+        int mid = l + (r-l)/2; //find mid 
+        mergeSort(arr,l,mid); // left 
+        mergeSort(arr,mid+1,r);//right
+        merge(arr,l , mid, r); //merge
+      
     }
-
-    public static void merge(int arr[], int si, int mid, int ei) {
-        int temp[] = new int[ei - si + 1];
-        int i = si;
-        int j = mid + 1;
-        int k = 0;
-
-        while (i <= mid && j <= ei) {
-            if (arr[i] <= arr[j]) {
-                temp[k++] = arr[i++];
-            } else {
-                temp[k++] = arr[j++];
+    void merge(int arr[],int l,int mid,int r){
+        int leftSize = mid-l +1;
+        int rightSize = r-mid;
+        int left[] = new int[leftSize];
+        int right[] = new int[rightSize];
+        // copy laft rnd raight arr
+        for(int i = 0;i<leftSize;i++){
+            left[i] = arr[l+i];
+        }
+        for(int i= 0;i<rightSize;i++){
+            right[i] = arr[mid+1+i];
+        }
+        int i = 0, j = 0, k = l;
+        while(i<leftSize && j <rightSize){
+            if(left[i] < right[j]){
+                arr[k] = left[i];
+                i++;
+            }else{
+                arr[k] = right[j];
+                j++;
             }
+            k++;
         }
-
-        while (i <= mid) {
-            temp[k++] = arr[i++];
+        while(i<leftSize){
+            arr[k] = left[i];
+            i++;
+            k++;
         }
-
-        while (j <= ei) {
-            temp[k++] = arr[j++];
+        while(j<rightSize){
+            arr[k] = right[j];
+            j++;
+            k++;
         }
-
-        for (k = 0, i = si; k < temp.length; k++, i++) {
-            arr[i] = temp[k];
-        }
-
+        
         
     }
 }
